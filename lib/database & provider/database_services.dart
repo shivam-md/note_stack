e58ca5f2e1e36b4e82181501b2f23ad4eec04db7
shivam-md <shivamsingh2451999@gmail.com>
2023-03-14 23:00:34 +0530

    first working sample

--- lib/database & provider/database_services.dart
+++ lib/database & provider/database_services.dart
@@ -1,37 +1,40 @@
-
+import 'package:flutter/widgets.dart';
 import 'package:note_stack/models/note.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class DatabaseServices {
+class DatabaseServices extends ChangeNotifier {
   // static named constructor.
   static final DatabaseServices instance = DatabaseServices._init();
+  // private constructor
+  DatabaseServices._init();
 
   // database instance
   static Database? _database;
 
-  // private constructor
-  DatabaseServices._init();
+  static List<Note> _list = [];
 
-  Future<Database> get database async{
-    if(_database != null){
+  List<Note> get notesList => _list;
+
+  Future<Database> get database async {
+    if (_database != null) {
       return _database!;
-    }
-    else{
+    } else {
       _database = await _initDB('notes.db');
       return _database!;
     }
   }
-  // initializing database
-  Future _initDB(String databaseName) async{
-    final dbPath = await getDatabasesPath();
-    final path = join(databaseName,dbPath);
 
-    return await openDatabase(path,version: 1,onCreate: _createDB);
+  // initializing database
+  Future _initDB(String databaseName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, databaseName);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
   // database creation
-  Future _createDB(Database db, int version) async{
+  Future _createDB(Database db, int version) async {
     const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
     const textType = 'TEXT NOT NULL';
     const boolType = 'BOOLEAN NOT NULL';
@@ -52,64 +55,76 @@ class DatabaseServices {
 
   // Creating Notes inside our database table
   Future<Note> create(Note note) async {
-    final db = await instance.database;
+    final db = await database;
 
     // this can be replaced using rawInsert property instead of insert and then we'll have to write the sql query to insert data inside our db table.
-    final id = await db.insert(TABLE_NAME,note.toJson()); // returns unique id for each insert
-    return note.copy(id: id);
+    final id = await db.insert(
+        TABLE_NAME, note.toJson()); // returns unique id for each insert
+    final create = note.copy(id: id);
+    readAllNotes();
+    //notifyListeners();
+    return create;
   }
 
   // Reading a specific note containing id
   Future<Note> readNote(int? id) async {
     final db = await instance.database;
 
-    final maps = await db.query(TABLE_NAME,
+    final maps = await db.query(
+      TABLE_NAME,
       columns: NoteFields.values,
       where: '${NoteFields.id} = ?',
       whereArgs: [id],
     );
 
-    if(maps.isNotEmpty) {
-      return Note.fromJson(maps.first); // returns first value that matches our query.
+    if (maps.isNotEmpty) {
+      return Note.fromJson(
+          maps.first); // returns first value that matches our query.
     } else {
       throw Exception('ID $id not found');
     }
   }
 
   // Reading all the notes inside of the notes table
-  Future<List<Note>> readAllNotes() async {
-    final db = await instance.database;
+  Future<void> readAllNotes() async {
+    final db = await database;
     const String orderBy = '${NoteFields.time} ASC';
     // final result = await db.rawQuery('SELECT * FROM $tableNotes ORDER BY $orderBy'); // same as below code just sql query
     final result = await db.query(TABLE_NAME, orderBy: orderBy);
     // map our result and then convert them to note from a json object and returns list of notes
-    return result.map((json) => Note.fromJson(json)).toList();
-
+    _list = result.map((json) => Note.fromJson(json)).toList();
+    notifyListeners();
   }
-  // update a note at a particular note using its id.
-  Future<int> updateNote(Note note) async{
+
+  // updating a note at a particular note using its unique id.
+  Future<int> updateNote(Note note) async {
     final db = await instance.database;
 
-    return db.update(
-        TABLE_NAME,
+    final update = db.update(
+      TABLE_NAME,
       note.toJson(),
       where: '${NoteFields.id} = ?',
       whereArgs: [note.id],
     );
+    readAllNotes();
+    return update;
   }
 
+  // deleting note
   Future<int> deleteNote(int id) async {
     final db = await instance.database;
 
-    return db.delete(
-        TABLE_NAME,
+    final delete = db.delete(
+      TABLE_NAME,
       where: '${NoteFields.id} = ?',
       whereArgs: [id],
     );
+    readAllNotes();
+    return delete;
   }
 
   // close database
-  Future close() async{
+  static Future close() async {
     final db = await instance.database;
     db.close();
   }
---
+++ lib/helpers/custom_dialog.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomDialogBox extends StatelessWidget {
+  const CustomDialogBox(
+      {Key? key,
+      required this.text,
+      this.firstButton,
+      this.secondButton,
+      required this.areButtonsFunctional}) : super(key: key);
+
+  final Widget text;
+  final Widget? firstButton;
+  final Widget? secondButton;
+  final bool areButtonsFunctional;
+
+  @override
+  Widget build(BuildContext context) {
+    if (areButtonsFunctional == true) {
+      return AlertDialog(
+        title: text,
+        actions: [
+          firstButton!,
+          secondButton!,
+        ],
+      );
+    } else {
+      return AlertDialog(
+        title: text,
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Close"),),
+        ],
+      );
+    }
+  }
+}
