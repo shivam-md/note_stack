1cd590522268f7f9746dc4018689bd1ac966bca6
shivam-md <shivamsingh2451999@gmail.com>
2023-03-14 23:00:34 +0530

    first working sample

---
+++ lib/helpers/custom_text_field.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class CustomTextEditor extends StatelessWidget {
+  const CustomTextEditor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
